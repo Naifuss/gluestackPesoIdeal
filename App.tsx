a755cbba500d3a0d71db29ccd257de290912a120
Naifuss <124268998+Naifuss@users.noreply.github.com>
2023-10-25 02:50:11 +0000

    Uau

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed"
-import { config } from "@gluestack-ui/config" // Optional if you want to use default theme
+import { config } from "@gluestack-ui/config" 
 import Main from "./src/screens/Main"
 
 
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
-      <Box width="100%" justifyContent="center" alignItems="center" height={"$full"}>
+      <Box width="100%" justifyContent="center" alignItems="center" h={"$full"}>
         <Main />
       </Box>
     </GluestackUIProvider>
--- src/screens/Main/index.jsx
+++ src/screens/Main/index.jsx
@@ -1,7 +1,19 @@
-import { Text } from "@gluestack-ui/themed";
+import { Box, Text, Center, InputField, Heading, FormControl, FormControlLabel, FormControlLabelText, Input } from "@gluestack-ui/themed";
 
 
 
 export default function Main(){
-    return <Text>Projeto Gluestack - Alterado</Text>
+    return <Box bg="$primary100" p="$2" h={200} w={300}>
+        <Center h={"$full"}>
+            <Heading>Peso Ideak</Heading>
+            <FormControl>
+                <FormControlLabel>
+                    <formControlLabelText>Peso</formControlLabelText>
+                </FormControlLabel>
+                <Input>
+                    <InputField />
+                </Input>
+            </FormControl>
+        </Center>
+    </Box>
 }
